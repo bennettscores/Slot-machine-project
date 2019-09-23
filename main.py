40371a92e660d729bf292e5d30e44a99176847d3
bennettscores <bennettscoresby1234@gmail.com>
2019-09-23 16:20:57 -0600

    updated but not fully functioning

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import random
 import time
 
+def main_menu():
+    print('Welcome to the Bottega Casino!')
 
-print('Welcome to the Bottega Casino!')
-
-print("The slot machines can take a $0.50, $1.00, or $5.00 bet and payout is: ")
-print("3 cherries: JACKPOT! 1:20")
-print("2 cherries: 1:10")
-print("1 cherry: 1:5")
-print("3 spades: 1:5")
-print("2 spades: 1:2")
-print("3 coins: 1:3")
-print("2 coins: 1:1")
-print("3 lemons: 1:1")
-time.sleep(2)
+    print("The slot machines can take a $0.50, $1.00, or $5.00 bet and payout is: ")
+    print("3 cherries: JACKPOT! 1:20")
+    print("2 cherries: 1:10")
+    print("3 spades: 1:5")
+    print("2 spades: 1:2")
+    print("3 coins: 1:3")
+    print("2 coins: 1:1")
+    print("3 lemons: 1:1.5")
+    time.sleep(2)
 
 
-bet_question = input('Would you like to play slots for a$0.50 b.$1.00 or c.$2.00?')
+    bet_question = input('Would you like to play slots for a$0.50 b.$1.00 or c.$2.00?')
+    slot_machine()
 
 def find_bet():
     if bet_question == 'a':
@@ -31,31 +31,60 @@ def find_bet():
 
 items = [
     "Cherries", "Cherries", 
-    # "Coin", "Coin", "Coin", "Coin", "Coin", "Coin", 
-    # "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", 
-    # "Spade", "Spade", "Spade", "Spade", "Spade", 
+    "Coin", "Coin", "Coin", "Coin", "Coin", "Coin", 
+    "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", 
+    "Spade", "Spade", "Spade", "Spade", "Spade", 
     ]
+def slot_machine():
+    column_one = random.choice(items)
+    column_two = random.choice(items)
+    column_three = random.choice(items)
 
-def column_one():
-    return random.choice(items)
+    result = f'{column_one} {column_two} {column_three}'
+    return result
+    payout()
 
-def column_two():
-    return random.choice(items)
 
-def column_three():
-    return random.choice(items)
-
-result = f'{column_one()} {column_two()} {column_three()}'
-print( result)
 
 def payout():
-    if result == "Cherries Cherries Cherries":
+    if result.count("Cherries") == 3:
         print("You won the Jackpot!")
         return 20
-    if result == "Spade Spade Spade":
+    elif result.count("Cherries") == 2:
+        return 10
+    elif result.count ("Spade") == 3:
         return 5
+    elif result.count("Spade") == 2:
+        return 2
+    elif result.count("Coin") ==3:
+        return 3
+    elif result.count("Coin") == 2:
+        return 1
+    elif result.count("Lemon") ==3:
+        return 1.5
+    elif result.count("Lemon") ==2:
+        return 0
+    winnings = payout() * find_bet()
+    print(f'You won ${winnings}')
+    play_again =input("Would you like to play again?")
+    restart()
 
-print(payout())
-print(find_bet())
-winnings = payout() * find_bet()
-print(f'You won ${winnings}')
+def restart():
+    if play_again == "yes":
+        main_menu()
+    elif play_again == "y":
+        main_menu
+    elif play_again == "Yes":
+        main_menu()
+    elif play_again == "Y":
+        main_menu
+    elif play_again == "no":
+        print("Thank you please come again!")
+    elif play_again == "n":
+        print("Thank you please come again!")
+    elif play_again == "No":
+        print("Thank you please come again!")
+    elif play_again == "N":
+        print("Thank you please come again!")
+
+main_menu()
