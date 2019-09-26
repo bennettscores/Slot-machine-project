71d65599369413bae38ec6d082873d8a004f9041
bennettscores <bennettscoresby1234@gmail.com>
2019-09-26 08:56:29 -0600

    Done just with maximum recursion bug

--- index.py
+++ index.py
@@ -1,12 +1,5 @@
-import random
+import numpy
 
-
-print('Welcome to the Bottega Casino! Would you like to play slots for $0.50, $1.00, or $5.00')
-
-items = ["Coin", "Lemon", "Diamond", "Spade", "Club", "Heart", "King"]
-
-
-def column_one():
-    return random.choice(items)
-    
-print(column_one())
+class SlotMachine:
+   def __init__(self, money_in_wallet = 0 ):
+        self.money
--- main.py
+++ main.py
@@ -1,90 +1,106 @@
 import random
 import time
+class SlotMachine:
 
-def main_menu():
-    print('Welcome to the Bottega Casino!')
-
-    print("The slot machines can take a $0.50, $1.00, or $5.00 bet and payout is: ")
-    print("3 cherries: JACKPOT! 1:20")
-    print("2 cherries: 1:10")
-    print("3 spades: 1:5")
-    print("2 spades: 1:2")
-    print("3 coins: 1:3")
-    print("2 coins: 1:1")
-    print("3 lemons: 1:1.5")
-    time.sleep(2)
+    def __init__(self):
+        self.items = [
+        "Cherries", "Cherries", 
+        "Coin", "Coin", "Coin", "Coin", "Coin", "Coin", 
+        "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", 
+        "Spade", "Spade", "Spade", "Spade", "Spade", 
+        ]
+        SlotMachine.main_menu(self)
+        self.bet = 0
 
 
-    bet_question = input('Would you like to play slots for a$0.50 b.$1.00 or c.$2.00?')
-    slot_machine()
+    def main_menu(self):
+        print('Welcome to the Bottega Casino!')
 
-def find_bet():
-    if bet_question == 'a':
-        return .5
+        print("The slot machines can take a $0.50, $1.00, or $5.00 bet and payout is: ")
+        print("3 cherries: JACKPOT! 1:20")
+        print("2 cherries: 1:10")
+        print("3 spades: 1:5")
+        print("2 spades: 1:2")
+        print("3 coins: 1:3")
+        print("2 coins: 1:1")
+        print("3 lemons: 1:1.5")
+
+        SlotMachine.asking_bet_question(self)
+
+    def asking_bet_question(self):
+        self.bet_question = input('Would you like to play slots for a$0.50 b.$1.00 or c.$2.00?')
+        if self.bet_question == 'a' or self.bet_question =='b' or self.bet_question =='c':
+            pass
+        else:
+            print('Please enter "a" for $0.50 "b" for $1.00 or "c" for $2.00')
+            self.bet_question
+            SlotMachine.asking_bet_question(self)
+        SlotMachine.slot_machine(self)
+
     
-    if bet_question == 'b':
-        return 1
-    
-    if bet_question == 'c':
-        return 2
-    
+    def slot_machine(self):
+        column_one = random.choice(self.items)
+        column_two = random.choice(self.items)
+        column_three = random.choice(self.items)
 
-items = [
-    "Cherries", "Cherries", 
-    "Coin", "Coin", "Coin", "Coin", "Coin", "Coin", 
-    "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", "Lemon", 
-    "Spade", "Spade", "Spade", "Spade", "Spade", 
-    ]
-def slot_machine():
-    column_one = random.choice(items)
-    column_two = random.choice(items)
-    column_three = random.choice(items)
+        result = f'{column_one} {column_two} {column_three}'
+        
+        
+        if result.count("Cherries") == 3:
+            print("You won the Jackpot!")
+            self.bet = 20
+        elif result.count("Cherries") == 2:
+            self.bet = 10
+        elif result.count ("Spade") == 3:
+            self.bet = 5
+        elif result.count("Spade") == 2:
+            self.bet = 2
+        elif result.count("Coin") ==3:
+            self.bet = 3
+        elif result.count("Coin") == 2:
+            self.bet = 1
+        elif result.count("Lemon") ==3:
+            self.bet = 1.5
+        elif result.count("Lemon") ==2:
+            self.bet = 0
+        
+        print(result)
+        time.sleep(1)
+        SlotMachine.payout_statement(self)
 
-    result = f'{column_one} {column_two} {column_three}'
-    return result
-    payout()
+
+    def payout_statement(self):
+        if self.bet_question == 'a':
+            winnings = self.bet * .5
+        if self.bet_question == 'b':
+            winnings = self.bet
+        if self.bet_question == 'c':
+            winnings = self.bet * 2
+        else:
+            print('Please enter "a" "b" or "c" for how much you want to bet')
+
+        print(f'You won ${winnings}')
+        self.play_again = input("Would you like to play again?")
+        SlotMachine.restart(self)
 
 
+    def restart(self):
+        if self.play_again == "yes":
+            SlotMachine.main_menu(self)
+        elif self.play_again == "y":
+            SlotMachine.main_menu(self)
+        elif self.play_again == "Yes":
+            SlotMachine.main_menu(self)
+        elif self.play_again == "Y":
+            SlotMachine.main_menu(self)
+        elif self.play_again == "no":
+            print("Thank you please come again!")
+        elif self.play_again == "n":
+            print("Thank you please come again!")
+        elif self.play_again == "No":
+            print("Thank you please come again!")
+        else:
+            print('Please write yes or no')
+            SlotMachine.restart(self)
 
-def payout():
-    if result.count("Cherries") == 3:
-        print("You won the Jackpot!")
-        return 20
-    elif result.count("Cherries") == 2:
-        return 10
-    elif result.count ("Spade") == 3:
-        return 5
-    elif result.count("Spade") == 2:
-        return 2
-    elif result.count("Coin") ==3:
-        return 3
-    elif result.count("Coin") == 2:
-        return 1
-    elif result.count("Lemon") ==3:
-        return 1.5
-    elif result.count("Lemon") ==2:
-        return 0
-    winnings = payout() * find_bet()
-    print(f'You won ${winnings}')
-    play_again =input("Would you like to play again?")
-    restart()
-
-def restart():
-    if play_again == "yes":
-        main_menu()
-    elif play_again == "y":
-        main_menu
-    elif play_again == "Yes":
-        main_menu()
-    elif play_again == "Y":
-        main_menu
-    elif play_again == "no":
-        print("Thank you please come again!")
-    elif play_again == "n":
-        print("Thank you please come again!")
-    elif play_again == "No":
-        print("Thank you please come again!")
-    elif play_again == "N":
-        print("Thank you please come again!")
-
-main_menu()
+SlotMachine()
